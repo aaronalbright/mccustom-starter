cc45058b0c109e34d0a728b9e1f37585f0076db3
aaronalbright <aaron.albright38@gmail.com>
2020-02-18 11:35:40 -0500

    replace helpers  with bling.js

---
+++ src/js/modules/bling.js
@@ -0,0 +1,17 @@
+const $ = document.querySelectorAll.bind(document);
+const $1 = document.querySelector.bind(document);
+
+Node.prototype.on = window.on = function(name, fn) {
+  this.addEventListener(name, fn);
+};
+
+NodeList.prototype.__proto__ = Array.prototype;
+
+NodeList.prototype.on = NodeList.prototype.addEventListener = function(
+  name,
+  fn
+) {
+  this.forEach(function(elem, i) {
+    elem.on(name, fn);
+  });
+};
