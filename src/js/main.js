d0dee688fd2781adbe616d8a6302a5c06eb4ba70
aaronalbright <aaron.albright38@gmail.com>
2021-01-27 13:40:35 -0500

    added svelte and new FuseNav

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,8 @@
-import FuseNav from './modules/fuse-nav';
-
-// Rearranges headers from lead image to story body
-// let immersive = document.querySelector('.immersive');
-// let headline = immersive.querySelector('.title');
-// let h6 = immersive.querySelector('h2.caps.h6');
-// let storyBody = document.querySelector('.story-body');
-// let header = storyBody.querySelector('.header');
-
-// header.insertBefore(headline, header.firstElementChild);
-// header.insertBefore(h6, header.firstElementChild);
+import './modules/bling.js';
+import FuseNav from './components/FuseNav.svelte'
 
 document.addEventListener('DOMContentLoaded', function () {
-  const readMore = $1('.fuse-read-more');
-  const nav = new FuseNav(readMore, {
-    seriesSequence: false,
-    removeCards: true,
-  });
-
-  nav.init();
+  new FuseNav({
+    target: $1('#FuseNav')
+  })
 });
