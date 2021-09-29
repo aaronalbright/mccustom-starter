deee7a85afd1c56da3760ce15794779b151f8a46
Aaron Albright <aalbright@mcclatchy.com>
2021-09-29 12:18:31 -0400

    package updates

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,22 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import postcssNesting from 'postcss-nesting';
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
-import fs from 'fs';
+// import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default ({ command }) => {
   if (command === 'build') {
     // weird workaround to get the embed file to be the main output with proper paths
-    fs.copyFileSync(resolve(__dirname, 'templates/embed.html'), 'embed.html');
+    // fs.copyFileSync(resolve(__dirname, 'templates/embed.html'), 'embed.html');
+    // fs.copyFileSync(resolve(__dirname, 'templates/simple-table.html'), 'simple-table.html');
   }
   return defineConfig({
     plugins: [
       svelte(),
       handlebars({
-        partialDirectory: resolve(__dirname, 'templates'),
+        partialDirectory: resolve(__dirname, 'partials'),
         context: {
-          title: 'CUE Custom Project',
+          title: 'cobb-timeline',
         },
       }),
     ],
@@ -28,8 +29,13 @@ export default ({ command }) => {
     },
     build: {
       rollupOptions: {
+        output: {
+          assetFileNames: "assets/[name][extname]",
+          chunkFileNames: "assets/[name].js",
+          entryFileNames: "assets/[name].js"
+        },
         input: {
-          embed: resolve(__dirname, 'embed.html'),
+          embed: resolve(__dirname, 'partials/embed.html')
         },
       },
     },
